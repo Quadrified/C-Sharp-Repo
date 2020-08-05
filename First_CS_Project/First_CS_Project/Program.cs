8bcce8b24358a0a343702b0a5e66db1b58340f6c
unknown <DSLAP-68@DSLAP-68.DSmart.com>
2020-08-05 17:53:49 +0530

    Added new CS file

---
+++ First_CS_Project/First_CS_Project/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace First_CS_Project
+{
+    public class Person
+    {
+        public string Name;
+        public int Age;
+        public bool HasPet;
+
+        public void greetPerson()
+        {
+            Console.WriteLine(Name + " is " + Age + "  and he has a pet " + HasPet);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+
+            person1.Name = "Omer Quadri";
+            person1.Age = 23;
+            person1.HasPet = true;
+
+            person1.greetPerson();
+        }
+    }
+}
